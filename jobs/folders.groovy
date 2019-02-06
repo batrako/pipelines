dd96fc582481387cf1351bb5062a4b8608bfd498
batrako <fsmetar@gmail.com>
2019-02-06 20:20:13 +0100

    folders

---
+++ jobs/folders.groovy
@@ -0,0 +1,15 @@
+folder('CaaS') {
+  description('Folder containing all jobs for CaaS integration')
+}
+
+folder('VM') {
+  description('Folder containing all jobs for elastic stack on Virtual Machines')
+}
+
+folder('filebeat') {
+  description('Folder containing all jobs for filebeat operation')
+}
+
+folder('filebeat/absis') {
+  description('Folder containing all jobs for ABSIS filebeat operation')
+}
