e4dfc26a9817ae6da1189f115b8ec1969007d58f
batrako <fsmetar@gmail.com>
2019-02-06 21:00:45 +0100

    pvs test

---
+++ jobs/CaaS_PV.groovy
@@ -0,0 +1,20 @@
+pipelineJob('CaaS/K8sPvCreate') {
+    description('pipeline for PV creation on CaaS')
+    parameters {
+        textParam('idegarApp', 'stack-elastic-arq','IDEGAR Aplication ID')
+        textParam('entorno', 'pro','Environment ID')
+        textParam('clusters', 'DC02','CaaS Clusters list for PVs/PVs Claim Deploy')
+    }
+    definition {
+        cpsScm { 
+            scm { 
+                git { 
+                    remote { url('https://github.com/batrako/pipelines') } 
+                    branches('master') 
+                    scriptPath('/scripts/pvs') 
+                    extensions { }  // required as otherwise it may try to tag the repo, which you may not want 
+                } 
+            } 
+        } 
+    }
+}
